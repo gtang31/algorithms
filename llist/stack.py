8b49f4cbbd808ceda1362e55243b40ab82a4b0c4
Gary Tang <garytang@Quris-MBP-2.webpass.net>
2017-05-24 08:37:57 -0700

    minor edits

--- llist/stack.py
+++ llist/stack.py
@@ -7,7 +7,8 @@ structure that supports the following operation/methods:
 - is_empty
 This script implements a stack using a singly linked list.
 """
-from LinkedList import LinkedList, Node
+from linkedlist import LinkedList, Node
+__author__ = "Gary Tang"
 
 
 class Stack(object):
@@ -18,7 +19,7 @@ class Stack(object):
     popping from the tail node would require O(n) time complexity
     """
     def __init__(self):
-        self.stack = LinkedList()
+        self._stack = LinkedList()
 
     def push(self, val):
         """
@@ -26,18 +27,18 @@ class Stack(object):
         @param val: item to push onto the stack
         """
         node = Node(val)
-        node.next = self.stack.head
-        self.stack.head = node
+        node.next = self._stack.head
+        self._stack.head = node
 
     def pop(self):
         """
         remove the item at the top of the stack
         @return node: node object
         """
-        if not self.stack.head:
+        if self.is_empty():
             raise Exception('Stack is already empty.')
-        node = self.stack.head
-        self.stack.head = self.stack.head.next
+        node = self._stack.head
+        self._stack.head = self._stack.head.next
         return node
 
     def peek(self):
@@ -45,13 +46,13 @@ class Stack(object):
         check value of item at the top of the stack
         @return: value of tail node
         """
-        if not self.stack.head:
+        if not self._stack.head:
             raise Exception('Stack is already empty.')
-        return self.stack.head.value
+        return self._stack.head.value
 
     def is_empty(self):
         """
         check if there are any items in the stack
         @return bool: if the head is None, then that means the ll is empty
         """
-        return self.stack.head is None
+        return self._stack.head is None
