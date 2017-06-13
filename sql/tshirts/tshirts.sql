a406dae879619ee1a8a488cb2c8610e8f8652ed4
Gary Tang <garytang@Quris-MBP-2.webpass.net>
2017-06-12 22:54:38 -0700

    added tshirts

---
+++ sql/tshirts/tshirts.sql
@@ -0,0 +1,17 @@
+select name,
+  sum(case when color = 'Red' then value else 0 end) Red,
+  sum(case when color = 'Green' then value else 0 end) Green,
+  sum(case when color = 'Blue' then value else 0 end) Blue
+from(
+    select color, Paul value, 'Paul' name
+      from tshirts
+      union all
+      select color, John value, 'John' name
+      from tshirts
+      union all
+      select color, Tim value, 'Tim' name
+      from tshirts
+      union all
+      select color, Eric value, 'Eric' name
+      from tshirts) src
+group by name
