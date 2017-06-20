8c1c6386e3ca98208b8457cf383896385cc5ea29
Gary Tang <garytang@Quris-MacBook-Pro-2.local>
2017-06-20 13:28:26 -0700

    added trie data structure

---
+++ tree/trie.py
@@ -0,0 +1,50 @@
+"""
+Implement the Trie data structure. A Trie is essentially a special-case of the
+tree data structure, where each node in the trie contains 26 child, one for
+each letter in the alphabet
+"""
+from tree.tree import Tree, Node
+__author__ = 'Gary Tang'
+
+
+class Trie(Tree):
+
+    def __init__(self):
+        super(Trie, self).__init__(K=26, from_list=['ROOT'])
+
+    def insert(self, word, node=None):
+        """
+        @param word. String to insert into the Trie
+        """
+        if not word:
+            # base case, we have inserted the entire word
+            return
+        if node is None:
+            # default call to method
+            node = self.root
+        for child in node.children:
+            if child.value == word[0]:
+                # prefix up to this letter was found
+                next_node = child
+                break
+        else:
+            node.children.append(Node(word[0]))
+            next_node = node.children[-1]
+        self.insert(word[1:], next_node)
+
+    def search(self, prefix, node=None):
+        """
+        @param prefix. string. Search whether the trie contains given prefix.
+        @return bool
+        """
+        if not prefix:
+            return True
+        if node is None:
+            node = self.root
+        for child in node.children:
+            if prefix[0] == child.value:
+                next_node = child
+                break
+        else:
+            return False
+        return self.startswith(prefix[1:], next_node) and True
