c0e14f95dbd325380eee08603fcf1eb28e0ffba6
Gary <gtang0131@gmail.com>
2018-08-10 10:58:12 -0700

    refactored code. Added tests

--- backtrack/phone_digits.py
+++ backtrack/phone_digits.py
@@ -10,34 +10,46 @@ __author__ = 'Gary Tang'
 
 
 class phone_digits:
+    """
+    Think of this as a DFS problem traversing a K-ary tree. Keep building out the combinations until we reach a "leaf".
+    """
+    def __init__(self):
+        self.map = {'2': 'abc'
+                   , '3': 'def'
+                   , '4': 'ghi'
+                   , '5': 'jkl'
+                   , '6': 'mno'
+                   , '7': 'pqrs'
+                   , '8': 'tuv'
+                   , '9': 'wxyz'}
+        self.output = []
+
     def letter_combinations(self, digits):
         """
-        Backtracking problem. Think of this as searching a tree
-        using DFS
         :type digits: str
         :rtype: List[str]
         """
-        output = []
         if not digits:
-            return output
-        mapping = {'2': 'abc',
-                  '3': 'def',
-                  '4': 'ghi',
-                  '5': 'jkl',
-                  '6': 'mno',
-                  '7': 'pqrs',
-                  '8': 'tuv',
-                  '9': 'wxyz'}
-        def backtrack(nums, s):
-            """help function for DFS"""
-            if not nums:
-                output.append(s)
-                return
-            for char in mapping.get(nums[0]):
-                backtrack(nums[1:], s+char)
-        backtrack(digits, '')
-        return output
+            return self.output
+
+        self._recurse('', digits)
+        return self.output
+
+    def _recurse(self, current_combo, remaining_digits):
+        """
+        DFS helper function
+        """
+        if not remaining_digits:
+            self.output.append(current_combo)
+            return
+
+        current_digit = remaining_digits[0]
+        for char in self.map[current_digit]:
+            self._recurse(current_combo+char, remaining_digits[1:])
 
 
+# test cases
 print(phone_digits().letter_combinations("5845"))
-print(phone_digits().letter_combinations("23"))
+assert(phone_digits().letter_combinations("9") == ['w', 'x', 'y', 'z'])
+assert(phone_digits().letter_combinations("23") == ["ad","ae","af","bd","be","bf","cd","ce","cf"])
+
