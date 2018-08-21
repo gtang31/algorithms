9947ba1ba0e4c08e410d21cd822740e4a90dfa96
Gary <gtang0131@gmail.com>
2018-08-21 15:27:35 -0700

    fix trie class

--- tree/trie.py
+++ tree/trie.py
@@ -1,17 +1,17 @@
 """
 Implement the Trie data structure. A Trie is essentially a special-case of the
-tree data structure, where each node in the trie contains 26 child, one for
+tree data structure, where each node in the trie contains up to 26 children, one for
 each letter in the alphabet
 """
 from tree.btree import BTree, Node
 __author__ = 'Gary Tang'
 
 
-class Trie(Tree):
+class Trie(object):
 
     def __init__(self):
-        # in databases, K should be a multiple of the hardware page size
-        super(Trie, self).__init__(K=32, from_list=['ROOT'])
+
+        self.root = Node("ROOT")
 
     def insert(self, word, node=None):
         """
@@ -19,33 +19,62 @@ class Trie(Tree):
         """
         if not word:
             # base case, we have inserted the entire word
+            node.children.append(Node('*'))
             return
+
         if node is None:
-            # default call to method
+            # set default node to root. Usually call by user
             node = self.root
+
         for child in node.children:
             if child.value == word[0]:
                 # prefix up to this letter was found
+                # move to next child
                 next_node = child
                 break
         else:
+            # prefix not found, so add new child
             node.children.append(Node(word[0]))
             next_node = node.children[-1]
+
         self.insert(word[1:], next_node)
 
-    def search(self, prefix, node=None):
+    def search(self, word, node=None):
+        if not word:
+            for child in node.children:
+                if child.value == '*':
+                    return True
+            return False
+
+        if not node:
+            node = self.root
+
+        for child in node.children:
+            if word[0] == child.value:
+                next_node = child
+                break
+        else:
+            return False
+
+        return self.search(word[1:], next_node)
+
+    def startswith(self, prefix, node=None):
         """
         @param prefix. string. Search whether the trie contains given prefix.
         @return bool
         """
         if not prefix:
             return True
+
         if node is None:
             node = self.root
+
         for child in node.children:
             if prefix[0] == child.value:
                 next_node = child
                 break
         else:
             return False
-        return self.search(prefix[1:], next_node) and True
+
+        return self.startswith(prefix[1:], next_node) and True
+
